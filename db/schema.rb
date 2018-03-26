01c7f80187b11e162f68ace02c54cf448886a3cf
ErikPerez312 <theerikperez@gmail.com>
2018-03-25 17:05:05 -0700

    Added Password, hash, and salt properties to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325165205) do
+ActiveRecord::Schema.define(version: 20180325235646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20180325165205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "age"
+    t.string "token"
+    t.string "password_hash"
+    t.string "password_salt"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end

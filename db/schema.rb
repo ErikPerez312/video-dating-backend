408ff887d090d3b8a7711541eba0fec5c01aecdd
ErikPerez312 <theerikperez@gmail.com>
2018-03-27 11:03:12 -0700

    Removed 'email' property from User

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327175903) do
+ActiveRecord::Schema.define(version: 20180327180207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "phone_number"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "age"
@@ -26,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180327175903) do
     t.string "password_salt"
     t.string "first_name"
     t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end

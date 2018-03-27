3bffa9139cb6a2eb0386bfee753120efbd9ba2a0
ErikPerez312 <theerikperez@gmail.com>
2018-03-27 10:55:59 -0700

    Removed name property from User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327175055) do
+ActiveRecord::Schema.define(version: 20180327175428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "phone_number"
     t.string "email"
     t.datetime "created_at", null: false

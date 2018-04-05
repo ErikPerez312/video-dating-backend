d9e311f67bfa0bb105f1aa9d6ec9ccb15d35a08e
ErikPerez312 <theerikperez@gmail.com>
2018-04-05 11:40:50 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   get "/token", to: "tokens#index"
   get "/session", to: "sessions#index"
-  get "/users/:user_id/matches",  to: "matches#index"
 
-  post "/matches", to: "matches#create"
+  get "/users/:user_id/matches",  to: "matches#index"
+  get "/matches/:id", to: "matches#show"
+  post "/users/:user_id/matches", to: "matches#create"
 end

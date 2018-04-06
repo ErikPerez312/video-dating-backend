0bdfe42e7c7043be83ee79e4705b8fc2edc5d587
ErikPerez312 <theerikperez@gmail.com>
2018-04-06 12:44:55 -0700

    Added validation and image types for paperclip

--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -1,3 +1,8 @@
 class ProfileImage < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image_file
+  validates_attachment :voice_file, content_type: {
+    content_type: ["image/jpeg", "image/gif", "image/png"]
+  }
 end

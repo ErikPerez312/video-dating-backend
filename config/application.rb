5166f76d242451ea74193009933ab05a01fade2b
ErikPerez312 <theerikperez@gmail.com>
2018-04-06 12:49:23 -0700

    Fixed issue with fetching ENV variables

--- config/application.rb
+++ config/application.rb
@@ -22,18 +22,19 @@ Bundler.require(*Rails.groups)
 module VideoDatingApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    Dotenv.load
 
+    config.load_defaults 5.1
     config.paperclip_defaults = {
       storage: :s3,
       s3_protocol: :https,
       url: ':s3_domain_url',
       path: '/:class/:attachment/:id_partition/:filename',
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        s3_region: ENV.fetch('AWS_REGION')
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        s3_region: ENV['AWS_REGION']
       }
     }
     config.api_only = true

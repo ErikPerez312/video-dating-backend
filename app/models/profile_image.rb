f83418f8aa34722e8e18ee03c4a1b672269deee4
ErikPerez312 <theerikperez@gmail.com>
2018-04-06 18:11:18 -0700

    Added profile image model

--- app/models/profile_image.rb
+++ app/models/profile_image.rb
@@ -2,7 +2,5 @@ class ProfileImage < ApplicationRecord
   belongs_to :user
 
   has_attached_file :image_file
-  validates_attachment :voice_file, content_type: {
-    content_type: ["image/jpeg", "image/gif", "image/png"]
-  }
+  validates_attachment :image_file, content_type: { content_type: ["image/jpeg", "image/gif", "image/png", "image/jpg"] }
 end

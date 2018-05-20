2542793d9bc0895da1c0adb8ffad1e6c731c8ef4
ErikPerez312 <theerikperez@gmail.com>
2018-05-20 12:34:44 -0700

    Updated 'seeking'/'gender' properties to be integer values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520182903) do
+ActiveRecord::Schema.define(version: 20180520193243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,10 +63,11 @@ ActiveRecord::Schema.define(version: 20180520182903) do
     t.string "password_salt"
     t.string "first_name"
     t.string "last_name"
-    t.string "gender"
     t.bigint "matches_id"
     t.boolean "is_online", default: false
     t.boolean "is_available", default: false
+    t.integer "seeking"
+    t.integer "gender"
     t.index ["matches_id"], name: "index_users_on_matches_id"
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
